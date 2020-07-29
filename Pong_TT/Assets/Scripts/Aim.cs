6644605ec0b6b14d7a6d056e47ac7c912011877f
fixmonah <fixmonah@gmail.com>
2020-07-29 17:18:40 +0300

    added script Aim

---
+++ Pong_TT/Assets/Scripts/Aim.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField] private Ball _ball;
+    [SerializeField] private GameObject _arrow;
+
+    private Camera _camera;
+    private bool _moveFlag;
+    private RaycastHit _raycastHit;
+    private float _distanceToBall;
+    private float _maxDistanceFromAimToTarget;
+    private Vector3 _scaleBefore;
+    
+    public void PrepareToFire()
+    {
+        _camera = GameController.instance.GetCamera();
+        _maxDistanceFromAimToTarget = GameController.instance.GetMaxDistanceFromAimToTarget();
+        _scaleBefore = transform.localScale;
+        _moveFlag = true;
+    }
+
+    public void Fire()
+    {
+        _moveFlag = false;
+        _ball.MoveToTarget(transform, _distanceToBall);
+        gameObject.SetActive(false);
+    }
+
+    private void MoveToNewPoint()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        Vector3 newPoint = _raycastHit.point;
+        
+        if (Physics.Raycast(ray, out _raycastHit)) {
+            newPoint.y = transform.position.y;
+        }
+
+        _distanceToBall = Vector3.Distance(_ball.transform.position, newPoint);
+        
+        if (_distanceToBall < _maxDistanceFromAimToTarget)
+        {
+            transform.position = newPoint;
+            _arrow.transform.localScale = _scaleBefore * _distanceToBall * 0.2f;
+        }
+    }
+
+    private void Update()
+    {
+        _arrow.transform.LookAt(_ball.transform);
+        
+        if (_moveFlag)
+        {
+            MoveToNewPoint();
+        }
+    }
+}
