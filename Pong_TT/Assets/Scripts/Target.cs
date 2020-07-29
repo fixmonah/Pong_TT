1cc2154a4e1d403c56866ebb8b9a8c33c4afa778
fixmonah <fixmonah@gmail.com>
2020-07-29 18:26:26 +0300

    Added collision on target

---
+++ Pong_TT/Assets/Scripts/Target.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Ball hitObject = other.gameObject.GetComponent<Ball>();
+        if (hitObject != null)
+        {
+            GameController.instance.GetGameField().TargetGetHit();
+            gameObject.SetActive(false);
+        }
+    }
+}
