17967557246bba974b2a45dc8d85059790b0f1a4
fixmonah <fixmonah@gmail.com>
2020-07-29 17:18:19 +0300

    added script Ball

--- Pong_TT/Assets/Scripts/Ball.cs
+++ Pong_TT/Assets/Scripts/Ball.cs
@@ -6,37 +6,16 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody _rigidbody;
 
-    [SerializeField] private float _velocity;
-    [SerializeField] private Transform _player;
-    
-    // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
-    
 
-    // Update is called once per frame
-    void Update()
+    public void MoveToTarget(Transform target, float speed)
     {
-        transform.LookAt(_player);
-        
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Vector3 force = _player.transform.position - transform.position;
-            force *= 10;
-            _rigidbody.AddForce(force);
-            
-            //_rigidbody.AddForceAtPosition(Vector3.forward, _player.transform.position,ForceMode.Impulse);
-            //_rigidbody.AddForce(1000,0,1000);
-            
-            //direction = _player.transform.position;
-            //direction *= -100;
-            //_rigidbody.AddForceAtPosition(direction, _player.transform.position);
-            
-            
-        }
-
-        _velocity = _rigidbody.velocity.magnitude;
+        Vector3 force = target.transform.position - transform.position;
+        force *= GameController.instance.GetBallSpeedFactor();
+        force *= speed;
+        _rigidbody.AddForce(force);
     }
 }
