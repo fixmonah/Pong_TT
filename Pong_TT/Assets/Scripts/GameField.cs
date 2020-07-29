937c5d3ba72d29513f75d9ae82878c4f95b77d06
fixmonah <fixmonah@gmail.com>
2020-07-29 17:21:37 +0300

    added script GameField

---
+++ Pong_TT/Assets/Scripts/GameField.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameField : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _target1,_target2,_target3;
+    [SerializeField] private Transform _enemyDefaultPoint;
+    [SerializeField] private Transform _ballDefaultPoint;
+    [SerializeField] private Transform _AimDefaultPoint;
+    [SerializeField] private Aim _aim;
+    [SerializeField] private Ball _ball;
+    [SerializeField] private Enemy _enemy;
+
+    #region MyRegion
+
+    public Aim GetAim()
+    {
+        return _aim;
+    }
+
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
